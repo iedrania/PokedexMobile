ba10926b5327aa64afc379acbf74c23d6cc94fff
Siti Iedrania <13519137@std.stei.itb.ac.id>
2022-05-21 13:50:55 +0700

    feat: add main data

---
+++ lib/main_data.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+ApiResultModel apiResultModelFromJson(String str) => ApiResultModel.fromJson(json.decode(str));
+
+String apiResultModelToJson(ApiResultModel data) => json.encode(data.toJson());
+
+class ApiResultModel {
+  ApiResultModel({
+    required this.count,
+    required this.next,
+    required this.previous,
+    required this.results,
+  });
+
+  int count;
+  String next;
+  dynamic previous;
+  List<Result> results;
+
+  factory ApiResultModel.fromJson(Map<String, dynamic> json) => ApiResultModel(
+    count: json["count"],
+    next: json["next"],
+    previous: json["previous"],
+    results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "count": count,
+    "next": next,
+    "previous": previous,
+    "results": List<dynamic>.from(results.map((x) => x.toJson())),
+  };
+}
+
+class Result {
+  Result({
+    required this.name,
+    required this.url,
+  });
+
+  String name;
+  String url;
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+    name: json["name"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "url": url,
+  };
+}
